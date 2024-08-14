9d5ae61454163a84bf17ee1dec14f40d0d23ceab
LightKazuto <ilhamsidi62@gmail.com>
2024-08-14 19:33:37 +0700

    feat: using useRouter on _app.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -25,7 +25,7 @@ const Navbar: React.FC<NavbarProps> = ({ userRole, onLogout }) => {
             <div className="hidden md:flex items-center space-x-1">
               {/* <a href="#" className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold">Home</a> */}
               <a
-                href="#"
+                href="/"
                 className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
               >
                 Home
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     if (storedUserRole) {
       setUserRole(storedUserRole);
     }
-  }, []);
+  }, [router.pathname]);
 
   const handleLogout = () => {
     localStorage.clear();
